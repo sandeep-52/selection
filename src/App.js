fa401a280a38f328f733c06278c13cf87c4f02c7
sandeep <sandeepyadav8452@gmail.com>
2022-03-10 15:56:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SelectionWidget from './Component/SelectionWidget';
+function App() {
+  return (
+    <>
+    <div>
+      <SelectionWidget/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/SelectionWidget.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const SelectionWidget = () => {
+  return (
+      <>
+    <div className='container'>
+   <button className='portal'>AwesomePortal</button>
+   <div className='left'>
+   <img src='https://5.imimg.com/data5/WW/LJ/KF/SELLER-89447710/nv-jeweler-traditional-kundan-and-beads-choker-necklace-set-for-women-blue--500x500.jpg' width="300px" alt='necklese'></img>
+   <div className='text'>
+<h1>Excellent necklace 6*8</h1>
+    <h1>Diamond AD</h1>
+    <p>ABC Sales Pvt Ltd</p><br></br>
+    <p2>The phrase "Lorem ipsum dolor sit amet consectetuer" appears in Microsoft Word online Help. 
+    This phrase has the appearance of an intelligent Latin idiom. Actually, it is nonsense.<br></br>
+          <br></br>During the 1500s, a printer adapted Cicero's text to develop a page of type samples.
+    Since then, the Latin-like text has been the printing industry's standard for fake, or dummy, text. 
+    Before electronic publishing, graphic designers had to mock. 
+     a more realistic way to indicate where text would go on a page.<br></br></p2>
+<button className='sandeep'>Buy Now</button>
+<button className='sandeep1'>All Right Reserved</button>
+</div>
+</div>
+</div>
+    
+    </>
+  )
+}
+
+export default SelectionWidget
